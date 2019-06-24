8dcc2f86792982ee649a370f5d8a77f4d3f3671d
Bennyson Brecht <49658184+bbrecht02@users.noreply.github.com>
2019-06-24 11:21:32 -0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,13 +1,14 @@
 def serie_fibonacci(n):
     fibonacci= [0, 1]
-    if n > 2:
+    if n >= 2:
         for i in range(2,n):
+            n= int(input("n?"))
             fibonacci.append(fibonacci[i-1]+fibonacci[i-2])
-            print("fibonacci")
+            print(fibonacci)
     else:
-        print("entrada invalida")
+        print("erro")
 
 
-serie_fibonacci(5)
+serie_fibonacci(10)
                              
                              

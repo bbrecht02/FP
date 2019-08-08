73c8e3142698960d5035763948820f55c303d9a5
Bennyson Brecht <49658184+bbrecht02@users.noreply.github.com>
2019-08-08 18:41:56 -0300

    Add files via upload

---
+++ listafinal/01.py
@@ -0,0 +1,4 @@
+salario= float(input("digite quanto tu ganha bença? "))
+porc= int(input("digite a porcentagem de aumento: "))
+print(f" Aumento: {(porc*salario)/100} novo salario: {(porc*salario)/100+salario}")
+
